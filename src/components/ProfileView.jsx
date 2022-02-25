256f6402e2b000d5dc3d5ccc2da92f2999a0eeaa
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-02-24 16:53:38 -0800

    saving progress

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function ProfileView({ profile }) {
+  const history = useHistory();
+  const handleSubmit = () => {
+    history.push('/profile/edit');
+  };
+
   return (
     <div>
       <h1>your profile</h1>
@@ -8,6 +14,7 @@ export default function ProfileView({ profile }) {
       <p>email: {profile.email}</p>
       <p>birthday: {profile.birthday}</p>
       <p>bio: {profile.bio}</p>
+      <button onClick={handleSubmit}>edit profile</button>
     </div>
   );
 }
